35f56b548f6388f647aaa4c889905832fa2beebf
Sarthak Patil <sarthaksp301@gmail.com>
2026-08-23 12:37:35 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from sqlalchemy.exc import OperationalError
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.api import health
 from app.api.routes import auth, job, resume, application
 from app.core.config import settings
@@ -8,19 +9,32 @@ from app.models import User, Resume, Job, Application
 from app.core.database import Base, engine
 from app.api.routes import user
 
+
 app = FastAPI(
     title=settings.APP_NAME,
     version="0.1.0",
 )
+
+
+# ======================================================
+# CORS
+# ======================================================
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://confident-amazement-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# ======================================================
+# Routes
+# ======================================================
 
 app.include_router(
     health.router,
@@ -35,6 +49,10 @@ app.include_router(resume.router)
 app.include_router(application.router)
 
 
+# ======================================================
+# Database
+# ======================================================
+
 @app.on_event("startup")
 def create_tables_on_startup():
     try:
@@ -43,6 +61,12 @@ def create_tables_on_startup():
         pass
 
 
+# ======================================================
+# Root
+# ======================================================
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to CareerPilot AI"}
+    return {
+        "message": "Welcome to CareerPilot AI"
+    }
